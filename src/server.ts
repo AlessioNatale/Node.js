a7024bc5035d20d36d319c6cf72686df91da83b0
AlessioNatale <alessionatale12@gmail.com>
2023-11-24 12:02:18 +0100

    es-14

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 
-import { log } from "console"
 import express from "express"
 import "express-async-errors"
 import morgan from "morgan"
+
+import {getAll, getOneById, create, updateById, deleteById} from "./controllers/planets.js"
 
 
 const app = express()
@@ -11,68 +12,17 @@ const port = 3000
 app.use(morgan("dev"))
 app.use(express.json())
 
-type Planet = {
-  id: number,
-  name: string,
-};
 
-type Planets = Planet[];
+app.get('/api/planets', getAll)
 
-let planets: Planets = [
-  {
-    id: 1,
-    name: "Earth",
-  },
-  {
-    id: 2,
-    name: "Mars",
-  },
-];
+app.get('/api/planets/:id',getOneById )
 
-app.get('/api/planets', (req, res) => {
-  res.status(200).json(planets)
-})
+app.post('/api/planets', create)
 
-app.get('/api/planets/:id', (req, res) => {
-  const {id} = req.params
-  // const id = req.params.id
-  const planet = planets.find((p) => p.id ===Number(id) ) 
-  if(!planet){
-    res.status(404).send("Planet not found")
-  }
-  res.status(200).json(planet)
-})
-
-app.post('/api/planets', (req, res) => {
-  const {id, name} = req.body
-  const newPlanet = {id, name}
-  planets = [...planets, newPlanet]
-
-  res.status(201).json({msg:"The planet was create"})
-
-  console.log(planets)
-
-})
-
-app.put('/api/planets/:id', (req, res) => {
-  const {id} = req.params
-  const {name} = req.body
-  planets = planets.map((p) => (p.id === Number(id) ? {...p, name} : p))
-
-  console.log(planets)
-
-  res.status(200).json({msg:"The planet was updated"})
-})
+app.put('/api/planets/:id', updateById )
 
 
-app.delete('/api/planets/:id', (req, res) => {
-  const {id} = req.params
-  planets = planets.filter((p) => p.id! == Number(id))
-
-  console.log(planets)
-
-  res.status(200).json({msg:"The planet was deleted"})
-})
+app.delete('/api/planets/:id', deleteById)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
